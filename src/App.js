21d6da630228c56c7e681c06323e69a8f942d6e7
ella-chetvertak <olegcetvertak037@gmail.com>
2022-08-30 15:58:26 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+const today = new Date()
+const alt = 'Logo img'
+console.log(today.getUTCDate())
+
+function Component(props) {
+  return (
+    <React.Fragment>
+      <img src={props.img} alt={props.alt} className='Component-image' />
+      <p>{props.text}</p>
+    </React.Fragment>
+  )
+}
+
+class ClassComponent extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <img src={this.props.img} alt={this.props.alt} className='Component-image' />
+        <p>{this.props.text}</p>
+      </React.Fragment>
+    )
+  }
+}
 
 function App() {
   return (
@@ -17,6 +42,8 @@ function App() {
         >
           Learn React
         </a>
+        <Component key={'func-comp'} img={logo} text={today.getUTCDate()} alt={alt}></Component>
+        <ClassComponent key={'class-comp'} img={logo} text={today.getUTCDate()} alt={alt}></ClassComponent> 
       </header>
     </div>
   );
